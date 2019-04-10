322fced73f948c17c6469ffe459dcbefad195608
alirezaghodrati <apps.ghodrati1377@gmail.com>
2019-04-10 17:27:29 +0430

    Now Detect InputCode Without Space Fix Codes

--- src/Utils/AnalysisUtil.kt
+++ src/Utils/AnalysisUtil.kt
@@ -8,105 +8,50 @@ import java.io.File
 object AnalysisUtil {
 
 
-    private var current_State: Int = 0 // Start State
+    private var current_State = 0 // Start State
     private var buffer: StringBuffer = StringBuffer()
 
     fun analysis(dfaTable: DFATable, tokens: MutableList<Token>, tokenTypes: HashSet<TokenType>) {
 
         println("Analysing inputCode...\r\n")
 
-        tokens.forEach {
-            println(it)
-        }
+        tokens
+            .forEach { token ->
 
+                token
+                    .srt
+                    .toLowerCase()
+                    .forEachIndexed { str_Index, token_char ->
 
-        /*// Read Code Form INPUT_CODE.txt
-        File("Input/INPUT_CODE.txt")
-            .takeIf { it.exists() }
-            ?.readLines()
-            ?.forEachIndexed { indexLine, line ->
+                        current_State = generateNextState(dfaTable, token_char)
+                        // println(current_State)
 
-                if (line.isNotEmpty()) {
-                    // Iterate All Chars
-                    val _line = line.toLowerCase()
-                    var i = 0
+                        if (isEndOfToken(dfaTable, str_Index, token)) {
 
-                    while (i != _line.length){
+                            getToken(current_State, tokenTypes)
+                                .also { type ->
+                                    if (type != null)
+                                        buffer.append(
+                                            "${type.Name} Detected at { LINE = ${token.line}" +
+                                                    " , INDEX = ${token.index} } " +
+                                                    " ->  \" ${token.srt} \""
+                                        )
+                                            .append("\r\n")
+                                }
 
-                        try {
-
-                            val currentChar = _line[i]
-
-                            //Save Start TokenType Index
-                            if (current_State == 0)
-                                start_index = i
-
-                            current_State = generateNextState(dfaTable, currentChar)
-
-
-                            if(isOp(line.getOrNull(i+1))){
-
-                                getToken(current_State, tokenTypeSet)
-                                    .also {
-                                        if (it != null)
-                                            buffer.append(
-                                                "${it.Name} Detected at { LINE = ${indexLine + 1}" +
-                                                        " , INDEX = ${start_index + 1} } " +
-                                                        " ->  \" ${line.substring(start_index, i + 1)} \""
-                                            )
-                                                .append("\r\n")
-                                    }
-
-                                current_State = 0
-                            }
-                            else if (isEndOfToken(dfaTable,line.getOrNull(i+1))) {
-
-                                getToken(current_State, tokenTypeSet)
-                                    .also {
-                                        if (it != null)
-                                            buffer.append(
-                                                "${it.Name} Detected at { LINE = ${indexLine + 1}" +
-                                                        " , INDEX = ${start_index + 1} } " +
-                                                        " ->  \" ${line.substring(start_index, i + 1)} \""
-                                            )
-                                                .append("\r\n")
-                                    }
-                            }
-
-                        } catch (e: Exception) {
-                            buffer.append(
-                                "Error Happened at { LINE = ${indexLine + 1} , INDEX = ${i + 1} } " +
-                                        " ->  \" ${line.substring(start_index, i + 1)} \""
-                            )
-                                .append("\r\n")
-                            // Reset current_State
                             current_State = 0
                         }
-
-                        i++
                     }
-
-                }
-                current_State = 0 // Reset For newline
             }
-
-            */
 
         println(buffer)
         saveOutput()
     }
 
-    private fun isEndOfToken(dfaTable: DFATable, nextChar: Char?): Boolean {
+    private fun isEndOfToken(dfaTable: DFATable, currentIndex: Int, token: Token): Boolean {
+        return isFinalState(dfaTable, current_State)
+                && currentIndex == token.srt.length - 1 // Detect When Token End
 
-        if (nextChar == null) return false
-
-        return nextChar.isWhitespace() && (isFinalState(dfaTable, current_State))
-
-    }
-
-    private fun isOp(nextChar: Char?): Boolean {
-        if (nextChar == null) return false
-        return !nextChar.isLetterOrDigit() && !nextChar.isWhitespace()
     }
 
     private fun isFinalState(dfaTable: DFATable, state: Int): Boolean {
